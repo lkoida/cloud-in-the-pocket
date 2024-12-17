9a569eddf3227cb6198a2daa72d89c5afd1dc3b5
lkoida <leonidkoida@gmail.com>
2024-12-17 15:03:55 +0200

    moved some scripts to the localstack ready stage

--- docker/localstack_scripts/0_create-secrets.sh
+++ docker/localstack_scripts/0_create-secrets.sh
@@ -8,30 +8,29 @@
 awslocal secretsmanager create-secret \
   --name "DB_CREDENTIALS" \
   --region "eu-west-2" \
-  --secret-string file://./scripts/example.secrets.json
+  --secret-string "{\"HOST\": \"postgres\",\"USER\": \"postgres\",\"PASSWORD\": \"postgres\",\"DATABASE\": \"service_db\",\"PORT\": \"5432\"}"
 
 awslocal secretsmanager create-secret \
- --name "API_SECRET" \
- --region "eu-west-2" \
- --secret-string "supersecret"
+  --name "API_SECRET" \
+  --region "eu-west-2" \
+  --secret-string "supersecret"
 
 awslocal secretsmanager create-secret \
- --name "REGION" \
- --region "eu-west-2" \
- --secret-string "eu-west-2"
+  --name "REGION" \
+  --region "eu-west-2" \
+  --secret-string "eu-west-2"
 
 awslocal secretsmanager create-secret \
- --name "ENDPOINT" \
- --region "eu-west-2" \
- --secret-string "localhost.localstack.cloud:4566"
+  --name "ENDPOINT" \
+  --region "eu-west-2" \
+  --secret-string "localhost.localstack.cloud:4566"
 
 awslocal secretsmanager create-secret \
- --name "BUCKET" \
- --region "eu-west-2" \
- --secret-string "todo-app-bucket"
+  --name "BUCKET" \
+  --region "eu-west-2" \
+  --secret-string "todo-app-bucket"
 
 awslocal secretsmanager create-secret \
---name "SALT" \
---region "eu-west-2" \
---secret-string ";:5}%(YD[M?R?gXrie9[eP4\\2o*+lqZE"
-
+  --name "SALT" \
+  --region "eu-west-2" \
+  --secret-string ";:5}%(YD[M?R?gXrie9[eP4\\2o*+lqZE"
